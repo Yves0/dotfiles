56bf7331853b1ba6d27364bc98305d1db4d80a4f
Yves <yves_luo@yahoo.com>
2019-02-17 16:45:33 +0800

    move xrdb .Xresource to .xinitrc

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,7 +11,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload top &
-    sleep 0.2s
+    sleep 0.5s
   done
 else
   polybar --reload top &
